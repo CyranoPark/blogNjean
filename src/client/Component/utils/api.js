b50cbf11896e4044123e12252f43e2f4eb02712c
park hanjun <park.hanjun@gmail.com>
2019-08-17 01:31:00 +0900

    fixed api.js indenting

--- src/client/Component/utils/api.js
+++ src/client/Component/utils/api.js
@@ -1,42 +1,53 @@
 export function getPostList(limit, sort, pageIndex) {
   const url = `/api/v1/articles?limit=${limit}&sort=${sort}&pageIndex=${pageIndex}`;
 
-  return fetch(url)
-    .then(res => res.json());
+  return (
+    fetch(url)
+      .then(res => res.json())
+  );
 }
 
 export function getAllTags() {
   const url = '/api/v1/tags';
 
-  return fetch(url)
-    .then(res => res.json());
+  return (
+    fetch(url)
+      .then(res => res.json())
+  );
 }
 
 export function getTagInfo(tagId) {
   const url = `/api/v1/tags/${tagId}`;
 
-  return fetch(url)
-    .then(res => res.json());
+  return (
+    fetch(url)
+      .then(res => res.json())
+  );
 }
 
 export function getCommentsOnArticle(articleId) {
   const url = `/api/v1/articles/${articleId}/comments`;
 
-  return fetch(url)
-    .then(res => res.json());
+  return (
+    fetch(url)
+      .then(res => res.json())
+  );
 }
 
 export function getArticleDetail(articleId) {
   const url = `/api/v1/articles/${articleId}`;
 
-  return fetch(url)
-    .then(res => res.json());
+  return (
+    fetch(url)
+      .then(res => res.json())
+  );
 }
 
 export function deleteArticle(articleId) {
   const url = `/api/v1/articles/${articleId}`;
-  return fetch(url, {
-    method: 'DELETE'
-  })
-    .then(res => res.json());
+  return (
+    fetch(url, {
+      method: 'DELETE'
+    }).then(res => res.json())
+  );
 }
